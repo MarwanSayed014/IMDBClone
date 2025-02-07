394d9a4fa9f9f6baea7135b053f9bff2318bb04c
MarwanSayed014 <marwansayed014@gmail.com>
2025-02-08 00:09:16 +0200

    Add EventHandlers

---
+++ IMDBClone/EventHandlers/SeriesEventHandler.cs
@@ -0,0 +1,27 @@
+﻿using IMDBClone.Models;
+using IMDBClone.Repos.Interfaces;
+
+namespace IMDBClone.EventHandlers
+{
+    public class SeriesEventHandler
+    {
+        public ISeasonRepo _seasonRepo { get; }
+
+        public SeriesEventHandler(ISeasonRepo seasonRepo)
+        {
+            _seasonRepo = seasonRepo;
+        }
+
+        public async Task CreateFirstSeason(Series series)
+        {
+            await _seasonRepo.CreateAsync(new Season
+            {
+                Number = 1,
+                ReleaseYear = series.ReleaseYear,
+                SeriesId = series.Id,
+                SeasonPrequelId = null
+            });
+            await _seasonRepo.SaveAsync();
+        }
+    }
+}
